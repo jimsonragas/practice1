832b53054bf3c5772745b6d65bb4219f6255ec1c
jimsonragas <jimsond.ragas@gmail.com>
2026-05-04 16:22:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-// PARALLAX
-document.addEventListener("mousemove", (e) => {
-  const profile = document.querySelector(".profile");
+const hero = document.querySelector(".hero");
 
-  let x = (window.innerWidth / 2 - e.pageX) / 50;
-  let y = (window.innerHeight / 2 - e.pageY) / 50;
+window.addEventListener("scroll", () => {
+  let scroll = window.scrollY;
 
-  profile.style.transform = `
-    translateX(-50%) rotateY(${x}deg) rotateX(${y}deg)
-  `;
-});
-
-
-// SCROLL FADE
-const sections = document.querySelectorAll(".section");
-
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.style.opacity = 1;
-      entry.target.style.transform = "translateY(0)";
-    }
-  });
-});
-
-sections.forEach(sec => {
-  sec.style.opacity = 0;
-  sec.style.transform = "translateY(50px)";
-  observer.observe(sec);
+  hero.style.transform = `scale(${1 + scroll * 0.0005})`;
+  hero.style.opacity = 1 - scroll * 0.0015;
 });
