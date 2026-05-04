6a734c1fc70c7932a322f3eace1b1013cf8d7faf
jimsonragas <jimsond.ragas@gmail.com>
2026-05-04 15:41:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+// PARALLAX EFFECT
 document.addEventListener("mousemove", (e) => {
-  const profile = document.querySelector(".profile img");
+  const profile = document.querySelector(".profile");
 
   let x = (window.innerWidth / 2 - e.pageX) / 40;
   let y = (window.innerHeight / 2 - e.pageY) / 40;
 
-  profile.style.transform = `translate(-50%, 0) rotateY(${x}deg) rotateX(${y}deg)`;
+  profile.style.transform = `
+    translateX(-50%) 
+    rotateY(${x}deg) 
+    rotateX(${y}deg)
+  `;
+});
+
+
+// FADE-IN ON LOAD
+window.addEventListener("load", () => {
+  document.querySelectorAll(".fade-in").forEach((el, i) => {
+    el.style.animationDelay = `${i * 0.2}s`;
+  });
 });
